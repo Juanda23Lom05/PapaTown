2ab4d79e68a81fbb845660cdf6932607bad92203
Yoel Gijon <yoel.canul.yacg@gmail.com>
2025-12-06 21:04:18 -0500

    feat(layout): agregar plantilla base HTML con Bootstrap y Google Fonts

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('titulo')</title>
+
+    {{-- CDN de Bootstrap  --}} 
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+    {{-- CDN de Google Fonts --}}
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+
+    {{-- Hojas de estilo CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}"> 
+</head>
+<body>
+    <div class="d-flex" id="wrapper">
+        
+        @include('components.sidebar')
+
+        <div class="flex-grow-1 w-100 d-flex flex-column" style="min-height: 100vh;">
+            
+            <main class="p-4">
+                @yield('content')
+            </main>
+
+        </div>
+
+    </div>
+    
+</body>
+</html>
